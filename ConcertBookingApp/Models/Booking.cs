86e88f91496c7cdbca5f2a6843a34232f50916f6
NoelStark <noel.k.stark@gmail.com>
2024-12-12 17:02:25 +0100

    Created Models

---
+++ ConcertBookingApp/Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertBookingApp.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; } = 0;
+        public int PerformanceId { get; set; } = 0;
+        public int UserId { get; set; } = new User().UserId;
+        public DateTime BookingDate { get; set; } = new DateTime();
+        public int SeatsBooked { get; set; } = 0;
+    
+    }
+}
